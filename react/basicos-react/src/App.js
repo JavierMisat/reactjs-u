e01b6539f032e3aec0358fee5c1c59b1c2a28c72
Javier Misat <rjmisal@misena.edu.co>
2018-11-22 12:31:15 -0500

    Usando react sin JSX

--- react/basicos-react/src/App.js
+++ react/basicos-react/src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return (
+            //React sin JSX
+            React.createElement(
+                'h1',
+                {id: 'titulo', className: 'encabezado'},
+                'Hola Mundo'
+            )
+
+        );
+    }
 }
 
 export default App;
