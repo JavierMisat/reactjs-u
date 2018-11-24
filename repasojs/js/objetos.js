dd641532c6e76aa18e3b64e86a1dedb6ba9f5884
Javier Misat <rjmisal@misena.edu.co>
2018-11-24 00:20:50 -0500

    Repaso - Object Construction

--- repasojs/js/objetos.js
+++ repasojs/js/objetos.js
@@ -1,1 +1,27 @@
-//
+/**
+ * @description Object Literal
+ * @type {string}
+ */
+let persona = {
+    nombre: 'Javier',
+    profesion: 'Developer',
+    edad: 500,
+    getDatos: () =>{
+        return `Nombre: ${persona.nombre} - Profesion: ${persona.profesion}`;
+    }
+};
+
+console.log(persona['nombre']);
+
+//Object constructions
+
+function Tarea(nombre, urgencia) {
+    this.nombre = nombre;
+    this.urgencia = urgencia;
+}
+
+//Crear una nueva tarea
+const tarea1 = new Tarea('Aprendiendo JS', 'alta');
+const tarea2 = new Tarea('Aprendiendo NODE', 'alta');
+
+console.log(tarea1, tarea2);
